48659cc8cfab93ad5c3d717dbb8f87caba881b64
unknown <junior.silva_costa@live.com>
2020-12-20 21:41:27 -0300

    teste

--- src/test/java/com/example/cadastroPessoa/CadastroPessoaTests.java
+++ src/test/java/com/example/cadastroPessoa/CadastroPessoaTests.java
@@ -1,10 +1,10 @@
-package com.example.minhaLojaDeGames;
+package com.example.cadastroPessoa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MinhaLojaDeGamesApplicationTests {
+class CadastroPessoaTests {
 
 	@Test
 	void contextLoads() {
